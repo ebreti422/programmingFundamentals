5b5c4d945f3549826f93ab659456a4b6c1b360af
ebreti422 <evanbreti@gmail.com>
2025-03-01 10:47:21 -0600

    reverse a string with recrusion

---
+++ codeToReverseAStringUsingRecrusion.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+void reverse(char *str, int index, int n)  
+{ 
+    if(index == n)      
+    { 
+        return; 
+    } 
+    char temp = str[index];   
+    reverse(str, index+1, n);   
+    cout << temp;               
+} 
+  
+int main() 
+{ 
+    char a[] = "HELLO WORLD"; 
+    int n = sizeof(a) / sizeof(a[0]); 
+    reverse(a, 0, n); 
+    return 0; 
+}
