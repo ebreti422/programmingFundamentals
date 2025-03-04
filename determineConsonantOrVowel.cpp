277cbc3d67b65d77488ce3c6c842e46013861832
ebreti422 <evanbreti@gmail.com>
2025-03-03 21:52:20 -0600

    code to determine vowel or consonant

---
+++ determineConsonantOrVowel.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    bool isLowercaseVowel, isUppercaseVowel;
+
+    cout << "Choose a letter from the alphabet: ";
+    cin >> c;
+
+    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c== 'u');
+
+    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c== 'U');
+
+    if (!isalpha(c))
+    printf("Error! Incorect input.");
+    else if ( isLowercaseVowel || isUppercaseVowel)
+    cout << c << " is a vowel.";
+    else
+    cout << c << " is a consonant.";
+
+    return 0;
+}
